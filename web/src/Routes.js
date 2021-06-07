6f5458788332e61abecfbffefac6d1f662cccd3a
Patrick Roth <patrockdev@icloud.com>
2021-06-07 17:35:02 +0200

    BlogLayout update

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -7,20 +7,26 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set } from '@redwoodjs/router'
-import BlogLayout from 'src/layouts/BlogLayout'
+import { Router, Route, Set, Private } from '@redwoodjs/router'
+import BlogPostLayout from 'src/layouts/BlogLayout'
 
 const Routes = () => {
   return (
     <Router>
-    <Set wrap={BlogLayout}>
-      <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
-      <Route path="/admin/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
-      <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
-      <Route path="/admin/posts" page={PostsPage} name="posts" />
-    </Set>
-    <Route notfound page={NotFoundPage} />
-  </Router>
+      <Set wrap={BlogPostLayout}>
+        <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
+        <Route path="/contact" page={ContactPage} name="contact" />
+        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/" page={HomePage} name="home" />
+      </Set>
+      <Private unauthenticated="home">
+        <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
+        <Route path="/admin/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
+        <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
+        <Route path="/admin/posts" page={PostsPage} name="posts" />
+      </Private>
+      <Route notfound page={NotFoundPage} />
+    </Router>
   )
 }
 
